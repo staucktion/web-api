c5a4135ab43260b5f3ca85764b48c37db1e37948
msteknoadam <40995274+msteknoadam@users.noreply.github.com>
2024-12-29 06:04:23 +0300

    Migrate temporary frontend

---
+++ frontend/README.md
@@ -0,0 +1,1 @@
+This is just a demo for now migrated from the old repo, should be switched to use React instead
--- src/router/Router.ts
+++ src/router/Router.ts
@@ -1,4 +1,5 @@
 import express from "express";
+import FrontendEndpoint from "src/endpoint/frontend/FrontendEntpoint";
 import HealthEndpoint from "src/endpoint/health/HealthEndpoint";
 import MailEndpoint from "src/endpoint/mail/MailEndpoint";
 import PhotoEndpoint from "src/endpoint/photo/PhotoEndpoint";
@@ -7,17 +8,20 @@ class Router {
 	private healthEndpoint: HealthEndpoint;
 	private photoEndpoint: PhotoEndpoint;
 	private mailEndpoint: MailEndpoint;
+	private frontendEndpoint: FrontendEndpoint;
 
 	constructor() {
 		this.healthEndpoint = new HealthEndpoint();
 		this.photoEndpoint = new PhotoEndpoint();
 		this.mailEndpoint = new MailEndpoint();
+		this.frontendEndpoint = new FrontendEndpoint();
 	}
 
 	public setupRoute(app: express.Application): void {
 		app.use(this.healthEndpoint.getRouter());
 		app.use(this.photoEndpoint.getRouter());
 		app.use(this.mailEndpoint.getRouter());
+		app.use(this.frontendEndpoint.getRouter());
 	}
 }
 
