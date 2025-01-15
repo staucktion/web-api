267e7b714ee557284892a4580bd5a05a0480b332
Ahmet Oğuz Ergin <ahmettoguz@hotmail.com>
2025-01-15 20:20:10 +0300

    #7 remove frontend endpoint

--- src/router/Router.ts
+++ src/router/Router.ts
@@ -1,5 +1,4 @@
 import express from "express";
-import FrontendEndpoint from "src/endpoint/frontend/FrontendEndpoint";
 import HealthEndpoint from "src/endpoint/health/HealthEndpoint";
 import MailEndpoint from "src/endpoint/mail/MailEndpoint";
 import PhotoEndpoint from "src/endpoint/photo/PhotoEndpoint";
@@ -8,13 +7,11 @@ class Router {
 	private healthEndpoint: HealthEndpoint;
 	private photoEndpoint: PhotoEndpoint;
 	private mailEndpoint: MailEndpoint;
-	private frontendEndpoint: FrontendEndpoint;
 
 	constructor() {
 		this.healthEndpoint = new HealthEndpoint();
 		this.photoEndpoint = new PhotoEndpoint();
 		this.mailEndpoint = new MailEndpoint();
-		this.frontendEndpoint = new FrontendEndpoint();
 	}
 
 	public setupRoute(app: express.Application): void {
@@ -22,9 +19,6 @@ class Router {
 		app.use(this.healthEndpoint.getRouter());
 		app.use(this.photoEndpoint.getRouter());
 		app.use(this.mailEndpoint.getRouter());
-
-		// Serve React app as fallback
-		app.use(this.frontendEndpoint.getRouter());
 	}
 }
 
