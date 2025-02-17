90a1454c436e95a561a480ac5d3c27885ba2373a
msteknoadam <40995274+msteknoadam@users.noreply.github.com>
2025-02-17 05:35:30 +0300

    Use authMiddleware in MailEndpoint

--- src/endpoint/mail/MailEndpoint.ts
+++ src/endpoint/mail/MailEndpoint.ts
@@ -1,18 +1,21 @@
 import express, { Router } from "express";
 import MailFacade from "src/facade/mail/mailFacade";
+import { AuthMiddleware } from "src/middleware/authMiddleware";
 
 class MailEndpoint {
 	private mailFacade: MailFacade;
 	private router: Router;
+	private authMiddleware: AuthMiddleware;
 
 	constructor() {
 		this.mailFacade = new MailFacade();
 		this.router = express.Router();
+		this.authMiddleware = new AuthMiddleware();
 		this.initializeRoutes();
 	}
 
 	private initializeRoutes(): void {
-		this.router.post("/mail/send", async (req, res) => {
+		this.router.post("/mail/send", this.authMiddleware.authenticateJWT, async (req, res) => {
 			await this.mailFacade.sendMail(req, res);
 		});
 	}
