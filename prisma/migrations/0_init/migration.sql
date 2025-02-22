9fbba5b8392d992d79052bfa7dd774ec08d82078
Ahmet Oğuz Ergin <ahmettoguz@hotmail.com>
2025-02-22 16:02:15 +0300

    [#16] prisma integration

---
+++ prisma/migrations/0_init/migration.sql
@@ -0,0 +1,148 @@
+-- CreateTable
+CREATE TABLE "auction" (
+    "id" BIGSERIAL NOT NULL,
+    "category_id" BIGINT NOT NULL,
+    "status_id" INTEGER NOT NULL,
+    "start_time" TIMESTAMP(6) NOT NULL,
+    "finish_time" TIMESTAMP(6) NOT NULL,
+    "is_deleted" BOOLEAN NOT NULL,
+    "created_at" TIMESTAMP(6) NOT NULL,
+    "updated_at" TIMESTAMP(6) NOT NULL,
+
+    CONSTRAINT "auction_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "bid" (
+    "id" BIGSERIAL NOT NULL,
+    "auction_id" BIGINT,
+    "user_id" BIGINT,
+    "amount" DECIMAL(15,2) NOT NULL,
+    "provision_id_on_bank" BIGINT,
+
+    CONSTRAINT "bid_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "category" (
+    "id" BIGSERIAL NOT NULL,
+    "name" VARCHAR(100) NOT NULL,
+    "status_id" INTEGER NOT NULL,
+    "address" VARCHAR(255) NOT NULL,
+    "location_id" BIGINT NOT NULL,
+    "valid_radius" DECIMAL(10,1) NOT NULL,
+    "is_deleted" BOOLEAN NOT NULL,
+    "created_at" TIMESTAMP(6) NOT NULL,
+    "updated_at" TIMESTAMP(6) NOT NULL,
+
+    CONSTRAINT "category_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "location" (
+    "id" BIGSERIAL NOT NULL,
+    "latitude" VARCHAR(100) NOT NULL,
+    "longitude" VARCHAR(100) NOT NULL,
+
+    CONSTRAINT "location_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "photo" (
+    "id" BIGSERIAL NOT NULL,
+    "title" VARCHAR(100),
+    "user_id" BIGSERIAL NOT NULL,
+    "auction_id" BIGINT,
+    "location_id" BIGSERIAL NOT NULL,
+    "category_id" BIGSERIAL NOT NULL,
+    "status_id" INTEGER NOT NULL,
+    "device_info" VARCHAR(255) NOT NULL,
+    "vote_count" INTEGER NOT NULL,
+    "is_deleted" BOOLEAN NOT NULL,
+    "created_at" TIMESTAMP(6) NOT NULL,
+    "updated_at" TIMESTAMP(6) NOT NULL,
+
+    CONSTRAINT "photo_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "status" (
+    "id" SERIAL NOT NULL,
+    "status" VARCHAR(100) NOT NULL,
+
+    CONSTRAINT "status_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "user" (
+    "id" BIGSERIAL NOT NULL,
+    "username" VARCHAR(100) NOT NULL,
+    "email" VARCHAR(100) NOT NULL,
+    "password" VARCHAR(255) NOT NULL,
+    "first_name" VARCHAR(100) NOT NULL,
+    "last_name" VARCHAR(100) NOT NULL,
+    "role_id" BIGSERIAL,
+    "status_id" INTEGER,
+    "is_deleted" BOOLEAN NOT NULL,
+    "created_at" TIMESTAMP(6) NOT NULL,
+    "updated_at" TIMESTAMP(6) NOT NULL,
+
+    CONSTRAINT "user_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "user_role" (
+    "id" BIGSERIAL NOT NULL,
+    "role" VARCHAR(100) NOT NULL,
+
+    CONSTRAINT "user_role_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "bid_provision_id_on_bank_key" ON "bid"("provision_id_on_bank");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "category_name_key" ON "category"("name");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "user_role_role_key" ON "user_role"("role");
+
+-- AddForeignKey
+ALTER TABLE "auction" ADD CONSTRAINT "auction_category_id_fkey" FOREIGN KEY ("category_id") REFERENCES "category"("id") ON DELETE SET NULL ON UPDATE NO ACTION;
+
+-- AddForeignKey
+ALTER TABLE "auction" ADD CONSTRAINT "auction_status_id_fkey" FOREIGN KEY ("status_id") REFERENCES "status"("id") ON DELETE SET NULL ON UPDATE NO ACTION;
+
+-- AddForeignKey
+ALTER TABLE "bid" ADD CONSTRAINT "bid_auction_id_fkey" FOREIGN KEY ("auction_id") REFERENCES "auction"("id") ON DELETE SET NULL ON UPDATE NO ACTION;
+
+-- AddForeignKey
+ALTER TABLE "bid" ADD CONSTRAINT "bid_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "user"("id") ON DELETE SET NULL ON UPDATE NO ACTION;
+
+-- AddForeignKey
+ALTER TABLE "category" ADD CONSTRAINT "category_location_id_fkey" FOREIGN KEY ("location_id") REFERENCES "location"("id") ON DELETE SET NULL ON UPDATE NO ACTION;
+
+-- AddForeignKey
+ALTER TABLE "category" ADD CONSTRAINT "category_status_id_fkey" FOREIGN KEY ("status_id") REFERENCES "status"("id") ON DELETE SET NULL ON UPDATE NO ACTION;
+
+-- AddForeignKey
+ALTER TABLE "photo" ADD CONSTRAINT "photo_auction_id_fkey" FOREIGN KEY ("auction_id") REFERENCES "auction"("id") ON DELETE SET NULL ON UPDATE NO ACTION;
+
+-- AddForeignKey
+ALTER TABLE "photo" ADD CONSTRAINT "photo_category_id_fkey" FOREIGN KEY ("category_id") REFERENCES "category"("id") ON DELETE SET NULL ON UPDATE NO ACTION;
+
+-- AddForeignKey
+ALTER TABLE "photo" ADD CONSTRAINT "photo_location_id_fkey" FOREIGN KEY ("location_id") REFERENCES "location"("id") ON DELETE SET NULL ON UPDATE NO ACTION;
+
+-- AddForeignKey
+ALTER TABLE "photo" ADD CONSTRAINT "photo_status_id_fkey" FOREIGN KEY ("status_id") REFERENCES "status"("id") ON DELETE SET NULL ON UPDATE NO ACTION;
+
+-- AddForeignKey
+ALTER TABLE "photo" ADD CONSTRAINT "photo_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "user"("id") ON DELETE SET NULL ON UPDATE NO ACTION;
+
+-- AddForeignKey
+ALTER TABLE "user" ADD CONSTRAINT "user_role_id_fkey" FOREIGN KEY ("role_id") REFERENCES "user_role"("id") ON DELETE SET NULL ON UPDATE NO ACTION;
+
+-- AddForeignKey
+ALTER TABLE "user" ADD CONSTRAINT "user_status_id_fkey" FOREIGN KEY ("status_id") REFERENCES "status"("id") ON DELETE SET NULL ON UPDATE NO ACTION;
+
