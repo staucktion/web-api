b4287f08743e91cf38bd3127bb27e4b243d2bc6f
msteknoadam <40995274+msteknoadam@users.noreply.github.com>
2025-03-02 06:17:40 +0300

    Refactor: Replace jsonStringifyBigint with sendJsonBigint utility

--- src/util/jsonStringifyBigint.ts
+++
@@ -1,7 +0,0 @@
-const jsonStringifyBigint = (param: any): any => {
-	return JSON.stringify(
-		param,
-		(key, value) => (typeof value === "bigint" ? value.toString() : value) // return everything else unchanged
-	);
-};
-export default jsonStringifyBigint;
